1015ecb8ce71d450fae98db656c5e36aef55b9b0
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-09-27 04:55:28 +0000

    First commit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+import requests as reqs
+from flask import Flask, render_template, request, url_for, redirect
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+app = Flask(__name__)       
+    
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        default_value = '127.0.0.1'
+        url = request.form.get('link', default_value)
+        return redirect(url_for('check_status', url=url))
+
+    
+    return render_template("index.html")
+
+
+@app.route("/<path:url>")
+def check_status(url):
+    res = reqs.get(url)
+    
+    return f"URL: {url}\nStatus: {res.status_code}"
+
+@app.route("/test")
+def show_test():
+    return render_template('test.html', site="GOOGLE")
+
+
+
+
